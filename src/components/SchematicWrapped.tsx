c75a827515549875544e76bca17ee73af1978417
Karthikeyan Natarajan <karthiknitt@gmail.com>
2025-02-27 14:17:51 +0530

    Ends Day 2

--- src/components/SchematicWrapped.tsx
+++ src/components/SchematicWrapped.tsx
@@ -3,37 +3,35 @@
 import { useSchematicEvents } from "@schematichq/schematic-react";
 import { useUser } from "@clerk/nextjs";
 import { useEffect } from "react";
-import { EmailAddress } from "@clerk/nextjs/server";
 const SchematicWrapped = ({ children }: { children: React.ReactNode }) => {
   const { identify } = useSchematicEvents();
   const { user } = useUser();
 
-  useEffect(()=>{
-    const userName=
-    user?.username??
-    user?.fullName??
-    user?.emailAddresses[0]?.emailAddress ??
-    user?.id;
+  useEffect(() => {
+    const userName =
+      user?.username ??
+      user?.fullName ??
+      user?.emailAddresses[0]?.emailAddress ??
+      user?.id;
 
-    if(user?.id){
-        identify({
-            //Company Level Key
-            company:{
-                keys:{
-                    id:user.id,
-                },
-                name:userName,
-            },
+    if (user?.id) {
+      identify({
+        //Company Level Key
+        company: {
+          keys: {
+            id: user.id,
+          },
+          name: userName,
+        },
 
-            // User Level Key
-            keys:{
-                id:user.id,
-            },
-            name:userName,
-        });}
-    },[user,identify]);
-
-  
+        // User Level Key
+        keys: {
+          id: user.id,
+        },
+        name: userName,
+      });
+    }
+  }, [user, identify]);
 
   return children;
 };
